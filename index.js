502efccf35ca007020f88962fe77833e5aedffa0
Tanawut Wattana <tanawut.wattana@gmail.com>
2019-07-13 15:49:45 +0700

    config nodemon for dev environment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("<H1>Hello World</H1>");
 });
 
 const PORT = process.env.PORT || 5000;
