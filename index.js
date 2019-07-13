ad9648a96d02717566cba35072c54d3ce476c108
Tanawut Wattana <tanawut.wattana@gmail.com>
2019-07-13 17:52:18 +0700

    set basic api for members

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const path = require("path");
 
 const app = express();
 
+//basic return json api
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "Bob Williams",
+    email: "bob@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "Shannon Jackson",
+    email: "shannon@gmail.com",
+    status: "active"
+  }
+];
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+
 //get route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "src", "public", "index.html"));
