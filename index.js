a30e496054d7afc4bd41ce4f6dfd109762e91900
Tanawut Wattana <tanawut.wattana@gmail.com>
2019-07-13 17:45:49 +0700

    set default  route and set static folder

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+//get route
 app.get("/", (req, res) => {
-  res.send("<H1>Hello World</H1>");
+  res.sendFile(path.join(__dirname, "src", "public", "index.html"));
 });
+
+//set static folder
+app.use(express.static(path.join(__dirname, "src", "public")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
