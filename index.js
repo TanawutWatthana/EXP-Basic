0bfa3153416bf970c6968924027297a996e7048e
Tanawut Wattana <tanawut.wattana@gmail.com>
2019-07-13 18:34:56 +0700

    add logger middleware

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
 const members = require("./members");
+const logger = require("./middleware/logger");
 
 const app = express();
+
+//Init middleware
+app.use(logger);
 
 //basic return json api
 app.get("/api/members", (req, res) => {
